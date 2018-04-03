8722b665fefce18040d2c6c3660e0345f8ce71d8
David Kinzer <dtkinzer@gmail.com>
2018-04-03 10:36:18 -0400

    Update so that it works with ApiGateway.

--- contact.go
+++ contact.go
@@ -4,25 +4,22 @@ import (
 	"bytes"
 	"fmt"
 	"github.com/SlyMarbo/gmail"
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"net/url"
 	"os"
 	"text/template"
 )
 
-type Request struct {
+type Contact struct {
 	Name    string `json:name`
 	Email   string `json:email`
 	Phone   string `json:phone`
 	Message string `json:message`
 }
 
-type Response struct {
-	Message string `json:message`
-	Ok      bool   `json:ok`
-}
-
-func mail(request Request) error {
-	subject := fmt.Sprintf("%s: [[%s]]", os.Getenv("CONTACT_EMAIL_SUBJECT"), request.Name)
+func mail(contact Contact) error {
+	subject := fmt.Sprintf("%s: [[%s]]", os.Getenv("CONTACT_EMAIL_SUBJECT"), contact.Name)
 	recipient := os.Getenv("CONTACT_EMAIL_RECIPIENT")
 	user := os.Getenv("CONTACT_EMAIL_USER")
 	password := os.Getenv("CONTACT_EMAIL_PASSWORD")
@@ -35,11 +32,10 @@ func mail(request Request) error {
 	Message:
 	{{.Message}}
 	`
-
 	// Create a new template and parse the email into it.
 	t := template.Must(template.New("email").Parse(email))
 	message := &bytes.Buffer{}
-	err := t.Execute(message, request)
+	err := t.Execute(message, contact)
 
 	if err != nil {
 		return err
@@ -48,26 +44,37 @@ func mail(request Request) error {
 	mail := gmail.Compose(subject, message.String())
 	mail.From = user
 	mail.Password = password
-
-	// Normally you'll only need one of these, but I thought I'd show both.
 	mail.AddRecipient(recipient)
 
 	return mail.Send()
 }
 
-func Handler(request Request) (Response, error) {
-	err := mail(request)
-	ok := true
+func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	message := "Message sent successfully."
+	status := 200
+
+	query, err := url.ParseQuery(request.Body)
 
 	if err != nil {
-		ok = false
-		message = "Failed to send message."
+		message = fmt.Sprint("Could not unmarshal the form query: ", err)
+		status = 500
 	}
 
-	return Response{
-		Message: message,
-		Ok:      ok,
+	err = mail(Contact{
+		Name:    query.Get("name"),
+		Email:   query.Get("email"),
+		Phone:   query.Get("phone"),
+		Message: query.Get("message"),
+	})
+
+	if err != nil {
+		message = "Failed to send message."
+		status = 500
+	}
+
+	return events.APIGatewayProxyResponse{
+		Body:       message,
+		StatusCode: status,
 	}, err
 }
 
